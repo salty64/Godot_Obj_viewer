809ddc02992d8246276eecab0a5c461866b848c0
salty64 <cedric.arrabie@univ-pau.fr>
2021-01-16 17:08:59 +0100

    click point 3D

--- Camera.gd
+++ Camera.gd
@@ -22,8 +22,12 @@ export (float, 0.05, 1.0) var zoom_speed = 0.05
 
 var mouse_rotate_button = false
 
+var position3D : Vector3
+var doubleclik : bool = false
+
 func _ready():
-	pass # Replace with function body.
+#	$InnerGimbal/Camera/RayCast/coin.show()
+	pass 
 
 
 func _unhandled_input(event):
@@ -40,10 +44,19 @@ func _unhandled_input(event):
 	if (event is InputEventMouseButton && event.button_index == BUTTON_RIGHT && !event.pressed):
 		mouse_rotate_button = false
 	
-	if (event is InputEventMouseButton && event.button_index == BUTTON_LEFT && event.pressed):
-		$InnerGimbal/Camera/RayCast.translation=Vector3(0,0,0)
+	doubleclik = false
+	
+	if (event is InputEventMouseButton && event.button_index == BUTTON_LEFT && event.doubleclick):
+		doubleclik= true
+		var position2D = get_viewport().get_mouse_position()
 		
+		position3D = $InnerGimbal/Camera.project_ray_origin(position2D)
+		print ("doubleclik -> ", position3D)
 		
+		position3D=$InnerGimbal/Camera.to_local(position3D)
+		$InnerGimbal/Camera/coin.translation= Vector3(position3D.x, position3D.y,$InnerGimbal/Camera/coin.translation.z)
+		
+
 	if (mouse_rotate_button and event is InputEventMouseMotion):
 		if event.relative.x != 0:
 			self.rotate_object_local(Vector3.UP, event.relative.x * mouse_sensitivity)
@@ -51,7 +64,17 @@ func _unhandled_input(event):
 		if event.relative.y != 0:
 			$InnerGimbal.rotate_object_local(Vector3.RIGHT, event.relative.y * mouse_sensitivity)
 
-
+func _physics_process(_delta):
+	if doubleclik:
+		$InnerGimbal/Camera/RayCast.translation= Vector3(position3D.x, position3D.y,$InnerGimbal/Camera/RayCast.translation.z)
+		
+		if $InnerGimbal/Camera/RayCast.is_colliding():
+#			$InnerGimbal/Camera/RayCast/coin.show()
+			
+			print ($InnerGimbal/Camera/RayCast.get_collision_point())
+		else:
+#			$InnerGimbal/Camera/RayCast/coin.hide()
+			pass
 
 func _process(_delta):
 	
--- Viewer.gd
+++ Viewer.gd
@@ -13,8 +13,15 @@ func _on_MenuButton_obj_selected(obj_name):
 	
 	$StaticBody/Object.mesh = load(path)
 	$StaticBody/CollisionShape.shape = $StaticBody/Object.mesh.create_trimesh_shape() 
-#	var mesh_outline = $Object.mesh.create_outline(0.001)
-#	print (mesh_outline)
+	
+
+#	var a = $StaticBody/Object.mesh.surface_get_arrays(0)
+#	var m = $StaticBody/Object.mesh.surface_get_material(0)
+#	$StaticBody/Object.mesh.surface_remove(0)
+#	$StaticBody/Object.mesh.add_surface_from_arrays(1, a)
+#	#if there's more than one surface, the surface idx should be the last
+#	$StaticBody/Object.mesh.surface_set_material(0, m)
+
 	
 	
 	var aabb 
