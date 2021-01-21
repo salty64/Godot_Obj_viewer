0f647777309f8abc3c3964f5faf99c21796226c0
salty64 <cedric.arrabie@univ-pau.fr>
2021-01-21 15:23:48 +0100

    Branche Salty

--- Viewer.gd
+++ Viewer.gd
@@ -4,7 +4,7 @@ extends Spatial
 
 func _ready():
 	pass
-	
+
 
 
 func _on_MenuButton_obj_selected(obj_name):
@@ -21,3 +21,13 @@ func _on_MenuButton_obj_selected(obj_name):
 	var obj_center = aabb.size / 2
 
 	$StaticBody.translation = - ( aabb.position + obj_center)
+		
+
+#	var mdt = MeshDataTool.new() 
+#	var nd = get_node("StaticBody/Object")
+#	var m = nd.get_mesh()
+#	#get surface 0 into mesh data tool
+#	mdt.create_from_surface(m, 0)
+#	for vtx in range(mdt.get_vertex_count()):
+#		var vert=mdt.get_vertex(vtx)
+#		print("global vertex: "+str(nd.global_transform.xform(vert)))
