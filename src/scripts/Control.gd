60caf09bb5a4fa4efacf78ace04e11183a42f661
salty64 <cedric.arrabie@univ-pau.fr>
2021-01-30 18:20:14 +0100

    Fix normal detection on fullscreen

--- src/scripts/Control.gd
+++ src/scripts/Control.gd
@@ -7,3 +7,15 @@ func _ready():
 
 
 
+
+
+func _on_ViewportContainer_resized():
+	printt('Size sur VP_Container:',$".".rect_size)
+	print("Viewport Resolution is: ", get_viewport_rect().size)
+	pass
+
+func _on_Viewport_size_changed():
+#	printt('Size sur VP_Container:',$".".rect_size)
+#	printt('Size sur VP:',$"../Viewport".rect_size)
+#	print("Viewport Resolution is: ", get_viewport_rect().size)
+	pass
